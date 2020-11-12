a10e8971c64566e066ac20deb9b957b5170cee43
Cedric Batina <shakagarvey@gmail.com>
2020-11-12 17:22:29 +0100

    pekocko first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const sauceRoutes = require("./routes/sauce");
+const userRoutes = require("./routes/user");
+const path = require("path"); // getting access to the server's path
+
+const app = express();
+mongoose
+  .connect(
+    "mongodb+srv://shakabatina:Yohannbatina2004@cluster0.zylwr.mongodb.net/test?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+app.use(bodyParser.json()); // bodyParser is used by all endpoints
+
+app.use("images", express.static(path.join(__dirname, "images")));
+app.use("/api/sauce", sauceRoutes);
+app.use("/api/auth", userRoutes);
+
+module.exports = app;
