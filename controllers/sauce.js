24f47b416f5f5c19f3e9415f72103125c72c3db6
Cedric Batina <shakagarvey@gmail.com>
2020-11-16 19:15:57 +0100

    getAll 2

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -141,7 +141,7 @@ exports.getOneSauce = (req, res, next) => {
     .catch((error) => res.status(404).json({ error }));
 };
 exports.getAllSauces = (req, res, next) => {
-  Sauce.find()
-    .then((Sauce) => res.status(200).json({ Sauce }))
+  Sauce.find({})
+    .then((sauces) => res.status(200).json({ sauces }))
     .catch((error) => res.status(400).json({ error }));
 };
